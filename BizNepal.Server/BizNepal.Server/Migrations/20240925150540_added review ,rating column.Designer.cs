4b6528d3c5eb66b52a251818f5516485ff7c99a5
utsab singh <utsabsingh5@gmail.com>
2024-09-25 21:29:35 +0545

    new migration

--- BizNepal.Server/BizNepal.Server/Migrations/20240925150540_added review ,rating column.Designer.cs
+++ BizNepal.Server/BizNepal.Server/Migrations/20240925150540_added review ,rating column.Designer.cs
@@ -4,6 +4,7 @@ using BizNepal.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BizNepal.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240925150540_added review ,rating column")]
+    partial class addedreviewratingcolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
