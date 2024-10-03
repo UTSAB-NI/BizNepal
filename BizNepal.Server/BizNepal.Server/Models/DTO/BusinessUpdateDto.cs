9086194f4ef016569f9dc93cc841f7ba9447729d
utsab singh <utsabsingh5@gmail.com>
2024-10-03 10:56:08 +0545

    Added BusinessUpdateDto

---
+++ BizNepal.Server/BizNepal.Server/Models/DTO/BusinessUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BizNepal.Server.Models.DTO
+{
+    public class BusinessUpdateDto
+    {
+        public string BusinessName { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
